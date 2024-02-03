237fc8be249d12392bde79b78a7bfca89b340d1e
Toluwanimi Sonuga <sonugatoluwanimi@gmail.com>
2024-02-03 21:02:01 +0100

    completed

--- list_recursion.py
+++ list_recursion.py
@@ -19,13 +19,21 @@ class ListRecursion:
     def remove_every_other(lst):
         if lst is None or lst ==[]:
             return lst
-        if len(lst) ==1:
+        if len(lst) == 1:
             return lst
         removed = ListRecursion.remove_every_other(lst[2:])
         return [lst[0]] + removed
     
     def maximum(lst):
-        pass
+        if lst is None or lst == []:
+            return lst
+        if len(lst) ==1:
+            return lst[0]
+        max_lst = ListRecursion.maximum(lst[1:])
+        if lst[0]> max_lst:
+            return lst[0]
+        else:
+            return max_lst
 
 if __name__ == "__main__":
    print_list = ListRecursion.print_sentence(['The', 'quick', 'brown', 'fox'])
@@ -34,3 +42,5 @@ if __name__ == "__main__":
    print(f'Reversed: {Reverse}')
    removed = ListRecursion.remove_every_other([1,2,3,4,5,6])
    print(removed)
+   max_list = ListRecursion.maximum([1,100,300,2, 6000])
+   print(f'max: {max_list}')
