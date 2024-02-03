5f8003ad9f53180fe122ed79f5e5e5891be476e7
Toluwanimi Sonuga <sonugatoluwanimi@gmail.com>
2024-02-03 20:50:23 +0100

    completed

--- list_recursion.py
+++ list_recursion.py
@@ -1,15 +1,36 @@
 class ListRecursion:
     def print_sentence(lst):
-        pass
+        if lst is None or " ":
+            return lst
+        if len(lst) ==1:
+            print (lst[0] + ". " ) # end of the sentence, add a period and space.
+        else:
+            print(lst[0]+ " ", end=" ")
+            ListRecursion.print_sentence(lst[1:])  # Recursive
+        
 
     def reverse(lst):
-        pass
+        if lst is None or lst ==[]:
+            return lst
+        reversed = ListRecursion.reverse(lst[1:])
+        return reversed + [lst[0]]
+        
 
     def remove_every_other(lst):
-        pass
-
+        if lst is None or lst ==[]:
+            return lst
+        if len(lst) ==1:
+            return lst
+        removed = ListRecursion.remove_every_other(lst[2:])
+        return [lst[0]] + removed
+    
     def maximum(lst):
         pass
 
 if __name__ == "__main__":
-    ListRecursion.print_sentence(['The', 'quick', 'brown', 'fox'])
+   print_list = ListRecursion.print_sentence(['The', 'quick', 'brown', 'fox'])
+   print(f"print: {print_list}")
+   Reverse = ListRecursion.reverse([1,2,3,4,5])
+   print(f'Reversed: {Reverse}')
+   removed = ListRecursion.remove_every_other([1,2,3,4,5,6])
+   print(removed)
